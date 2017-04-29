a222a52da22ad1662c37db4fb722d2295820952d
Carlos Esteban Raffellini <carlosraffellini@gmail.com>
2017-04-29 16:13:00 +0100

    modify scaleway script

--- provisioning/scaleway/host.bash
+++ provisioning/scaleway/host.bash
@@ -6,9 +6,5 @@ apt-get update -q
 apt-get install  -y -q git
 apt-get install  -y -q vagrant
 
-echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" >> /etc/apt/sources.list
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-apt-get update -q
-apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" virtualbox-5.1
+bash ./virtualbaox_install.bash
 
